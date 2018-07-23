535e7e18548d556440d512f849407bd43710d342
Yongjai <dev.yongjai@gmail.com>
2018-07-23 21:50:37 +0900

    day 1: Add transition event

--- Drum-kit/display.js
+++ Drum-kit/display.js
@@ -2,6 +2,13 @@ window.addEventListener('keydown', playSound);
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`div[data-value="${e.keyCode}"]`);
+
     audio.currentTime = 0;
+    key.classList.add("playing");
     audio.play();
+
+    setTimeout(function () {
+        key.classList.remove('playing');
+    }, 100);
 }
