5a0ec42c96ded5db327093bb0786089057219adb
Yongjai <dev.yongjai@gmail.com>
2018-07-26 21:50:20 +0900

    day 3: Beautify codes

--- Playground/display.js
+++ Playground/display.js
@@ -1,10 +1,7 @@
 const inputs = document.querySelectorAll('.controller input');
 
 function handleUpdate() {
-//    console.log(this.value);
     const suffix = this.dataset.sizing || '';
-    console.log(suffix);
-    //   document.documentElement.style.setProperty(`--${this.}`, this.value + suffix);
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
