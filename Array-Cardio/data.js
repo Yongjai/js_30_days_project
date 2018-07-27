c5d13744f69cf938c9df47741376e725ee1757c1
Yongjai <dev.yongjai@gmail.com>
2018-07-27 19:53:51 +0900

    day 4: Complete array cardio

--- Array-Cardio/data.js
+++ Array-Cardio/data.js
@@ -73,9 +73,6 @@ const inventors = [{
     }
 ];
 
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
-
-
 // TODO: 아래 조건에 맞춘 코드 작성하기
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
@@ -96,23 +93,26 @@ console.log(years);
 // 4. How many years did all the inventors live?
 const lives = inventors.reduce((total, inventor) => {
     return total + (inventor.passed - inventor.year);
-},0);
+}, 0);
 console.log(lives);
 
 // 5. Sort the inventors by years lived
-const oldest = inventors.sort(function(a, b) {
+const oldest = inventors.sort( (a, b) => {
     const c = a.passed - a.year;
     const d = b.passed - b.year;
     return c - d
-  });
-  console.log(oldest);
-
+});
+console.log(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const lastNames = inventors.sort( (a, b) => {
+    const c = a.last.toLowerCase();
+    const d = b.last.toLowerCase();
 
-// 8. Reduce Exercise
-// Sum up the instances of each of these
+    return c > d ? 1 : -1
+});
+console.log(lastNames);
